62d7df037e84dfa3f9a28e7c1300c0b3c463a4c7
Anitra Griffin <anitra.griffin99@gmail.com>
2022-11-07 19:42:04 -0500

    pushing final tutorial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('connection', () => {
+        io.emit('chat message', 'a user connected')
+        console.log('a user connected');
+
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+    // socket.on('disconnect', () => {
+    //     io.emit('a user disconnected');
+    //     console.log('user disconnected');
+    // });
+});
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
